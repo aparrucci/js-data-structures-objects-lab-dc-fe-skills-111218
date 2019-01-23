7635f4ada969aca47b6118f35f03c45d8739c9cc
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-23 22:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let driver = {
+  name: "Sam",
+  address: "11 Broadway"};
+  
+function updateDriverWithKeyAndValue(obj, key, value) {
+const newDriver = {...driver};
+newDriver ['driver'] = 'Sam';
+return newDriver;
+  }
+  
+  
+function destructivelyUpdateDriverWithKeyAndValue(){
+    
+  }
+  
+  
+function deleteFromDriverByKey() {
+  
+}
+
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
